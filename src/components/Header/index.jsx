c6fafae2a6bab4e3af8eec1bde079f0be8578a7f
avivianedev <developer.viviane@gmail.com>
2025-12-10 20:03:00 -0300

    Finaliza sessao hero-navbar mobile

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,33 +3,64 @@ import "./Header.modules.scss";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { MdOutlineClose } from "react-icons/md";
 
-
-
 const Header = () => {
+  const [showMenuMobile, setShowMenuMobile] = useState(false);
 
-    const [showmenu, SetShowMenu] = useState(false)
+  const showResponsiveMenu = (event) => {
+    event.preventDefault();
+    setShowMenuMobile(!showMenuMobile);
+    
+  };
 
-    const showResponsiveMenu = (event) => {
-        event.preventDefault();
-        SetShowMenu(!showmenu)
-    }
-    return (
-        <header className={showmenu ? "header-wrapper bg-header" : "header-wrapper"}>
-            {showmenu ? <MdOutlineClose onClick={showResponsiveMenu} className="menu-responsive-hamburger close" />
-                :
-                <GiHamburgerMenu onClick={showResponsiveMenu} className="menu-responsive-hamburger" />
-            }
 
-            <h1 className={showmenu ? "logo show-menu-responsive" : "logo"}>room</h1>
+  return (
+    <header
+      className="header-wrapper"
+    >        
+      {showMenuMobile ? (
+        <MdOutlineClose
+          onClick={showResponsiveMenu}
+          className="icon-responsive-hamburger close"
+        />
+      ) : (
+        <GiHamburgerMenu
+          onClick={showResponsiveMenu}
+          className="icon-responsive-hamburger"
+        />
+      )}
 
-            <ul className={showmenu ? "nav-list show-menu-responsive" : "nav-list"}>
-                <li className="nav-list-item"><a href="">home<hr className="hover-navlink" /></a></li>
-                <li className="nav-list-item"><a href="">shop<hr className="hover-navlink" /></a></li>
-                <li className="nav-list-item"><a href="">about<hr className="hover-navlink" /></a></li>
-                <li className="nav-list-item"><a href="">contact<hr className="hover-navlink" /></a></li>
-            </ul>
-        </header>
-    )
-}
+      <h1 className={showMenuMobile ? "invisible" : "logo"}>room</h1>
 
-export default Header
+      <nav className={showMenuMobile ? "navigation-menu" : "invisible"}>
+        <ul className="nav-list">
+          <li className="nav-list-item">
+            <a href="">
+              home
+              <hr className="hover-navlink"  />
+            </a>
+          </li>
+          <li className="nav-list-item">
+            <a href="">
+              shop
+              <hr className="hover-navlink"  />
+            </a>
+          </li>
+          <li className="nav-list-item">
+            <a href="">
+              about
+              <hr className="hover-navlink" />
+            </a>
+          </li>
+          <li className="nav-list-item">
+            <a href="">
+              contact
+              <hr className="hover-navlink" />
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
