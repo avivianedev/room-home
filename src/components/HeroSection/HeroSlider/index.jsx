b2af8f5a311f8acef8f786dee6ed1c2318c2c40e
avivianedev <developer.viviane@gmail.com>
2025-12-14 01:43:58 -0300

    fix: Corrige delay de passagem de slide

--- src/components/HeroSection/HeroSlider/index.jsx
+++ src/components/HeroSection/HeroSlider/index.jsx
@@ -2,35 +2,35 @@ import SliderControls from "../../SliderControls";
 import "./HeroSlider.modules.scss";
 import { slidesContent } from "../HeroContent/data";
 
-const HeroSlider = ({indexSlider, handlePrev, handleNext}) => {
-  const currentSlide = slidesContent  
-  
+const HeroSlider = ({ indexSlider, handlePrev, handleNext }) => {
+  const currentSlide = slidesContent;
+
   return (
     <section className="hero-slider-container">
       <img
+        key={indexSlider}
         src={`/assets/images/hero-mobile_0${indexSlider}.png`}
-        alt={currentSlide[indexSlider-1].alt}
+        alt={currentSlide[indexSlider - 1].alt}
         className="hero-image mobile"
       />
 
       <img
+        key={indexSlider}
         src={`/assets/images/hero-tablet_0${indexSlider}.png`}
-        alt={currentSlide[indexSlider-1].alt}
+        alt={currentSlide[indexSlider - 1].alt}
         className="hero-image tablet"
         hidden
       />
 
       <img
+        key={indexSlider}
         src={`/assets/images/hero-desktop_0${indexSlider}.png`}
-        alt={currentSlide[indexSlider-1].alt}
+        alt={currentSlide[indexSlider - 1].alt}
         className="hero-image desktop"
         hidden
       />
 
-      <SliderControls 
-        handlePrev={handlePrev}
-        handleNext={handleNext}
-      />
+      <SliderControls handlePrev={handlePrev} handleNext={handleNext} />
     </section>
   );
 };
